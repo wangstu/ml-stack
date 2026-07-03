f864312e30e0dce236e365d0947eb33d264c2bd4
wangstu <hit_wch@126.com>
2026-07-04 01:50:09 +0800

    ci: multi-arch Docker build (amd64 + arm64)
    - Add docker/setup-qemu-action for cross-platform emulation
    - Add docker/setup-buildx-action for multi-architecture builder
    - Use docker/build-push-action with platforms linux/amd64,linux/arm64
    - Enable GitHub Actions cache (gha) for faster rebuilds
    - Add amd64-only fallback step on multi-arch failure
    - Update Nix flake to support both x86_64-linux and aarch64-linux
      via flake-utils.eachDefaultSystem
    - Move docker login before build step for push access

--- container-images/init/scripts/flake.nix
+++ container-images/init/scripts/flake.nix
@@ -1,24 +1,26 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      sys = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${sys};
-    in
-    {
-      defaultPackage.${sys} = pkgs.buildEnv {
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        defaultPackage = pkgs.buildEnv {
         name = "ml-stack-init";
         paths = with pkgs; [
           openssh
-          python39Packages.supervisor
+          supervisor
           inotify-tools
           docker
           unzip
           coreutils
         ];
-      };
-    };
+        };
+      }
+    );
 }
